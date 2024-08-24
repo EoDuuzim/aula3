ba770ffff748b95fe1bf25bab41583db929f4381
EoDuuzim <162379540+EoDuuzim@users.noreply.github.com>
2024-08-23 21:46:32 -0300

    Add files via upload

---
+++ edu1Aula/script.js
@@ -0,0 +1,9 @@
+const ntmAdicionarTask = document.getElementById('addButton');
+
+btnAdicionarTask.setAttribute('onclick', 'addTask()');
+
+function addTask()
+{
+    console.lpg("Eai amogos");
+}
+btnAdicionarTask.setAttribute('onclick', 'addTask()');
